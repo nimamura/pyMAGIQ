047b322a261beca5bc11289c1711e485e2cd156e
Naoto Imamura <nimamura@Kiyomis-MacBook-Pro-3.local>
2020-01-22 15:03:41 +0900

    change library from cartopy to Basemap

--- pyMAGIQ/vis/plotUSmap.py
+++ pyMAGIQ/vis/plotUSmap.py
@@ -15,34 +15,32 @@ NI, 2018
 #=================================================================
  #imports
 #=================================================================
+import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
+import cartopy.feature as cfeature
+import cartopy
+import cartopy.crs as ccrs
 import xml.etree.ElementTree as ET
 
 #************************************************************************/
 def plotMap(datadir,xmin,xmax,ymin,ymax,latlist,lonlist,ratelist,fpath):
 
-    sc = 12
+    sc = 8
+    extent = [xmin, xmax, ymin, ymax]
 
     fig = plt.figure(figsize=(16,9), dpi=150, facecolor='w', edgecolor='k')
 
     # Map
-    m = Basemap(projection='merc',
-                     resolution='l',
-                     llcrnrlon=xmin,
-                     llcrnrlat=ymin,
-                     urcrnrlon=xmax,
-                     urcrnrlat=ymax)
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_extent(extent)
 
-    m.drawcoastlines(color='lightgray')
-    m.drawcountries(color='lightgray')
-    m.fillcontinents(color='white', lake_color='#eeeeee', zorder = 0);
-    m.drawmapboundary(fill_color='#eeeeee')
+    ax.coastlines(resolution='110m')
+    ax.add_feature(cartopy.feature.BORDERS, edgecolor='gray', linewidth=0.5)
+    ax.add_feature(cartopy.feature.LAND, edgecolor='black',facecolor='white')
+    ax.add_feature(cartopy.feature.LAKES, edgecolor='black',facecolor='white')
 
     cm = plt.cm.get_cmap('rainbow',5)
-    x,y = m(lonlist, latlist)
-    sca= m.scatter(x,y,c=ratelist, s=sc,cmap=cm)
-
+    sca = plt.scatter(lonlist, latlist, c=ratelist, s=sc, cmap=cm)
     plt.colorbar(sca, shrink=0.6)
     plt.clim(0.5,5.5)
     fig.savefig(fpath,format='eps')
